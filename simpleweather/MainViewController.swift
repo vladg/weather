9a515123037f4e0450ba54279e4be6d1ee53c2aa
Vlad Glabai <vlad.glabai@gmail.com>
2017-03-25 11:58:45 -0500

    create project

---
+++ simpleweather/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleweather
+//
+//  Created by Vladislav Glabai on 3/25/17.
+//  Copyright © 2017 Vladislav Glabai. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
